a97d3f0da2edc9c0f1880ce9a28cee26e27585ee
Sakura-Sx <raquitin@protonmail.com>
2025-12-05 23:31:34 +0100

    day 2

---
+++ 2.py
@@ -0,0 +1,30 @@
+# part 1
+solution = 0
+input_file = open("2.txt", "r")
+
+input_list = input_file.read().split(",")
+for pair in input_list:
+    pair = pair.split("-")
+    for i in range(int(pair[0]), int(pair[1]) + 1):
+        if len(str(i)) & 1 == 0:
+            if int(str(i)[:len(str(i))//2]) == int(str(i)[len(str(i))//2:]):
+                solution += i
+
+print(solution)
+
+
+# part 2
+solution = 0
+input_file = open("2.txt", "r")
+
+input_list = input_file.read().split(",")
+for pair in input_list:
+    pair = pair.split("-")
+    for i in range(int(pair[0]), int(pair[1]) + 1):
+        for j in range(1, int(len(str(i)))):
+            if len(str(i)) % j == 0:
+                if all(str(i)[0:j] == str(i)[k:k+j] for k in range(0, len(str(i)), j)):
+                    solution += i
+                    break
+
+print(solution)
